ac75c7c52405e1e3d5c20a7c807e71137e4bf780
Job <info@jobbomodels.nl>
2020-03-25 13:13:15 +0100

    Add Motor driver files

---
+++ BLINK_LED/Core/Inc/Motor_drive.h
@@ -0,0 +1,13 @@
+/*
+ * Motor_drive.h
+ *
+ *  Created on: 25 mrt. 2020
+ *      Author: Job Heijlighen
+ */
+
+#ifndef INC_MOTOR_DRIVE_H_
+#define INC_MOTOR_DRIVE_H_
+
+
+
+#endif /* INC_MOTOR_DRIVE_H_ */
---
+++ BLINK_LED/Core/Src/Motor_driver.c
@@ -0,0 +1,8 @@
+/*
+ * Motor_driver.c
+ *
+ *  Created on: 25 mrt. 2020
+ *      Author: Job Heijlighen
+ */
+
+#include "Motor_driver.h"
