eb81fe07fe115196cb827c1460fc97a11c97dc3e
nlrstem <Rick.Stemkens@vanderlande.com>
2020-04-22 13:32:55 +0200

    Added pwm .c and .h

---
+++ DevelopmentBoardBaseFile/Core/Inc/pwm.h
@@ -0,0 +1,13 @@
+/*
+ * pwm.h
+ *
+ *  Created on: 22 apr. 2020
+ *      Author: nlrstem
+ */
+
+#ifndef INC_PWM_H_
+#define INC_PWM_H_
+
+
+
+#endif /* INC_PWM_H_ */
---
+++ DevelopmentBoardBaseFile/Core/Src/pwm.c
@@ -0,0 +1,12 @@
+/*
+ * pwm.c
+ *
+ *  Created on: 22 apr. 2020
+ *      Author: nlrstem
+ */
+
+#include "tim.h"
+#include "pwm.h"
+
+
+
