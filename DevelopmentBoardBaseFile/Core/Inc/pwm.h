5be32f79a70924dc23e0e6d3411ee1bfb350b38d
nlrstem <Rick.Stemkens@vanderlande.com>
2020-04-24 09:24:58 +0200

    PWM led prototypes functions

--- DevelopmentBoardBaseFile/Core/Inc/pwm.h
+++ DevelopmentBoardBaseFile/Core/Inc/pwm.h
@@ -8,6 +8,20 @@
 #ifndef INC_PWM_H_
 #define INC_PWM_H_
 
+#include "main.h"
 
+enum PWM {PWM_1, PWM_2, PWM_3, PWM_4, PWM_5, PWM_6};
+
+void StartPWM_1(void);
+void StartPWM_2(void);
+void StartPWM_3(void);
+void StartPWM_4(void);
+void StartPWM_5(void);
+void StartPWM_6(void);
+void StartPWM_All(void);
+
+long map(long x, long in_min, long in_max, long out_min, long out_max);
+
+void LedBrightness(uint8_t PWM_Channel, uint16_t map_val);
 
 #endif /* INC_PWM_H_ */
--- DevelopmentBoardBaseFile/Core/Src/pwm.c
+++ DevelopmentBoardBaseFile/Core/Src/pwm.c
@@ -5,8 +5,95 @@
  *      Author: nlrstem
  */
 
-#include "tim.h"
 #include "pwm.h"
+#include "tim.h"
 
+/**
+ * @brief  Start PWM Channel 1
+ */
+void StartPWM_1(void) {
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+}
 
+/**
+ * @brief  Start PWM Channel 2
+ */
+void StartPWM_2(void) {
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+}
 
+/**
+ * @brief  Start PWM Channel 3
+ */
+void StartPWM_3(void) {
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
+}
+
+/**
+ * @brief  Start PWM Channel 4
+ */
+void StartPWM_4(void) {
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+}
+
+/**
+ * @brief  Start PWM Channel 5
+ */
+void StartPWM_5(void) {
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+}
+
+/**
+ * @brief  Start PWM Channel 6
+ */
+void StartPWM_6(void) {
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+}
+
+/**
+ * @brief  Start all PWM channels
+ */
+void StartPWM_All(void) {
+	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_ALL);
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_ALL);
+}
+
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+/**
+ * @brief  Fixed led brightness
+ * @param  PWM Channel DevBoard PWM_x labels
+ * @param  Percentage from 0 to 100%
+ */
+void LedBrightness(uint8_t PWM_Channel, uint16_t Brightness) {
+
+	long map_val = map(Brightness, 0, 100, 0, 1024);
+
+	switch (PWM_Channel) {
+	case PWM_1:
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, map_val);
+		break;
+
+	case PWM_2:
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, map_val);
+		break;
+
+	case PWM_3:
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, map_val);
+		break;
+
+	case PWM_4:
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, map_val);
+		break;
+
+	case PWM_5:
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, map_val);
+		break;
+
+	case PWM_6:
+		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, map_val);
+		break;
+	}
+}
