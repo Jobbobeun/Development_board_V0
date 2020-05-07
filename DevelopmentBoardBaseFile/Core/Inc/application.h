c632983fffb20ca1fdcbf673b34f93659adc7193
Job <info@jobbomodels.nl>
2020-05-07 09:13:58 +0200

    Demo Blink an LED

--- DevelopmentBoardBaseFile/Core/Inc/application.h
+++ DevelopmentBoardBaseFile/Core/Inc/application.h
@@ -7,7 +7,10 @@
 
 #ifndef INC_APPLICATION_H_
 #define INC_APPLICATION_H_
+#include "stdint.h"
 
 void application(void);
+uint8_t i;
+
 
 #endif /* INC_APPLICATION_H_ */
--- DevelopmentBoardBaseFile/Core/Src/application.c
+++ DevelopmentBoardBaseFile/Core/Src/application.c
@@ -8,9 +8,30 @@
 
 #include "application.h"
 #include "adc.h"
+#include "gpio.h"
 
 
 void application(void){
 
+for (i = 1 ; i < 8 ; i++){
+
+	if (i ==1 ){
+	HAL_GPIO_TogglePin(GPIOB, OUT_8_Pin);
+	} else if (i ==2 ){
+	HAL_GPIO_TogglePin(GPIOB, OUT_9_Pin);
+	}else if (i ==3 ){
+	HAL_GPIO_TogglePin(GPIOB, OUT_3_Pin);
+	}else if (i ==4 ){
+	HAL_GPIO_TogglePin(GPIOB, OUT_4_Pin);
+	}else if (i ==5 ){
+	HAL_GPIO_TogglePin(GPIOB, OUT_5_Pin);
+	}else if (i ==6 ){
+	HAL_GPIO_TogglePin(GPIOB, OUT_6_Pin);
+	}else if (i ==7 ){
+	HAL_GPIO_TogglePin(GPIOA, OUT_7_Pin);
+	}
+
+	HAL_Delay(100);
+}
 
 }
