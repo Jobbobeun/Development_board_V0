6c1a699b3801675a46cbb4276b9cf5e508b5cd02
Job <info@jobbomodels.nl>
2020-05-12 11:30:34 +0200

    All leds on.

--- DevelopmentBoardBaseFile/Core/Src/application.c
+++ DevelopmentBoardBaseFile/Core/Src/application.c
@@ -15,26 +15,8 @@
 
 void application(void){
 
-/*
-for (i = 1 ; i < 8 ; i++){
 
-	if (i ==1 ){
-	HAL_GPIO_TogglePin(GPIOB, OUT_1_Pin);
-	} else if (i ==2 ){
-	HAL_GPIO_TogglePin(GPIOB, OUT_2_Pin);
-	}else if (i ==3 ){
-	HAL_GPIO_TogglePin(GPIOB, OUT_3_Pin);
-	}else if (i ==4 ){
-	HAL_GPIO_TogglePin(GPIOB, OUT_4_Pin);
-	}else if (i ==5 ){
-	HAL_GPIO_TogglePin(GPIOB, OUT_5_Pin);
-	}else if (i ==6 ){
-	HAL_GPIO_TogglePin(GPIOB, OUT_6_Pin);
-	}else if (i ==7 ){
-	HAL_GPIO_TogglePin(GPIOA, OUT_7_Pin);
-	}
-	HAL_Delay(100);
-}
-*/
+
+
 
 }
--- DevelopmentBoardBaseFile/Core/Src/init.c
+++ DevelopmentBoardBaseFile/Core/Src/init.c
@@ -19,10 +19,16 @@ void DevBoardInit(void){
 	StartPWM_6();
 
 	PWM_Duty_Cycle(PWM_1, 100);
-	PWM_Duty_Cycle(PWM_2, 80);
-	PWM_Duty_Cycle(PWM_3, 60);
-	PWM_Duty_Cycle(PWM_4, 40);
-	PWM_Duty_Cycle(PWM_5, 20);
-	PWM_Duty_Cycle(PWM_6, 10);
-
+	PWM_Duty_Cycle(PWM_2, 100);
+	PWM_Duty_Cycle(PWM_3, 100);
+	PWM_Duty_Cycle(PWM_4, 100);
+	PWM_Duty_Cycle(PWM_5, 100);
+	PWM_Duty_Cycle(PWM_6, 100);
+	HAL_GPIO_TogglePin(GPIOB, OUT_1_Pin);
+	HAL_GPIO_TogglePin(GPIOB, OUT_2_Pin);
+	HAL_GPIO_TogglePin(GPIOB, OUT_3_Pin);
+	HAL_GPIO_TogglePin(GPIOB, OUT_4_Pin);
+	HAL_GPIO_TogglePin(GPIOB, OUT_5_Pin);
+	HAL_GPIO_TogglePin(GPIOB, OUT_6_Pin);
+	HAL_GPIO_TogglePin(GPIOA, OUT_7_Pin);
 }
