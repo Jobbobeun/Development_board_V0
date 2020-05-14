7c19e74f494b67267ad5ac14499730be058797a1
wojtekheuvel <wojtek.heuvel@gmail.com>
2020-05-14 14:26:36 +0200

    add serial communication

---
+++ DevelopmentBoardBaseFile/Core/Inc/serialCom.h
@@ -0,0 +1,13 @@
+/*
+ * serialCom.h
+ *
+ *  Created on: 14 mei 2020
+ *      Author: wojte
+ */
+
+#ifndef INC_SERIALCOM_H_
+#define INC_SERIALCOM_H_
+
+void serialcom_init(void);
+
+#endif /* INC_SERIALCOM_H_ */
---
+++ DevelopmentBoardBaseFile/Core/Src/serialCom.c
@@ -0,0 +1,16 @@
+/*
+ * serialCom.c
+ *
+ *  Created on: 14 mei 2020
+ *      Author: wojtek
+ */
+
+#include "serialCom.h"
+#include "stdio.h"
+
+uint8_t serial;
+
+void serialcom_init(void)
+{
+
+}
