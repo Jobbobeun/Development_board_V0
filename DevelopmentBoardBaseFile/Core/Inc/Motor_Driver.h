69e90b318eb7b1e3e587df754e5080c47cdac696
AndroidzZzZ <robvankempen21@gmail.com>
2020-05-27 12:02:01 +0200

    create motor drive C en H file

---
+++ DevelopmentBoardBaseFile/Core/Inc/Motor_Driver.h
@@ -0,0 +1,13 @@
+/*
+ * Motor_Driver.h
+ *
+ *  Created on: 27 mei 2020
+ *      Author: Rob
+ */
+
+#ifndef INC_MOTOR_DRIVER_H_
+#define INC_MOTOR_DRIVER_H_
+
+
+
+#endif /* INC_MOTOR_DRIVER_H_ */
---
+++ DevelopmentBoardBaseFile/Core/Src/Motor_Driver.c
@@ -0,0 +1,8 @@
+/*
+ * Motor_Driver.c
+ *
+ *  Created on: 27 mei 2020
+ *      Author: Rob
+ */
+
+
