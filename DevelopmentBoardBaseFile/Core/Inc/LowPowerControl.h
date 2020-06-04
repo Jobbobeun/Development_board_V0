fc953877dbc28131b18c8d53473475c3911ae5fe
Job <info@jobbomodels.nl>
2020-06-04 10:39:48 +0200

    Added files for low power control.

---
+++ DevelopmentBoardBaseFile/Core/Inc/LowPowerControl.h
@@ -0,0 +1,13 @@
+/*
+ * LowPowerControl.h
+ *
+ *  Created on: Jun 4, 2020
+ *      Author: Job Heijlighen
+ */
+
+#ifndef INC_LOWPOWERCONTROL_H_
+#define INC_LOWPOWERCONTROL_H_
+
+
+
+#endif /* INC_LOWPOWERCONTROL_H_ */
---
+++ DevelopmentBoardBaseFile/Core/Src/LowPowerControl.c
@@ -0,0 +1,9 @@
+/*
+ * LowPoerControl.c
+ *
+ *  Created on: Jun 4, 2020
+ *      Author: Job Heijlighen
+ */
+
+
+#include "LowPowerControl.h"
