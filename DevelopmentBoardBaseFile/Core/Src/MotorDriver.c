c99cc41282904646bbb1f4502973694218931c7a
Job <info@jobbomodels.nl>
2020-06-21 19:41:09 +0200

    If there is an error in the motor controller. All the motor driver variables go back to default.

--- DevelopmentBoardBaseFile/Core/Src/MotorDriver.c
+++ DevelopmentBoardBaseFile/Core/Src/MotorDriver.c
@@ -15,6 +15,8 @@
 uint8_t MotorDriverActualDirection[3];
 uint8_t MotorDriverActualSpeed[3];
 uint8_t StepperStateVariable[2];
+uint8_t MotorOutputStatus[4];
+uint8_t FirstMotorStart[2];
 
 bool ControlMotor_CW(uint8_t MotorOutputNumber);
 bool ControlMotor_CCW(uint8_t MotorOutputNumber);
@@ -28,6 +30,12 @@ void MotordriverIni(void)
 
 	MotorDriverActualDirection[Motor1] = 255;
 	MotorDriverActualDirection[Motor2] = 255;
+	MotorOutputStatus[0] = 0;
+	MotorOutputStatus[1] = 0;
+	MotorOutputStatus[2] = 0;
+	MotorOutputStatus[3] = 0;
+	FirstMotorStart[Motor1] = true;
+	FirstMotorStart[Motor2] = true;
 }
 
 bool Motor(uint8_t MotorOutputNumber, uint8_t Direction, uint8_t Speed)
@@ -41,6 +49,7 @@ bool Motor(uint8_t MotorOutputNumber, uint8_t Direction, uint8_t Speed)
 			MotorDriverActualSpeed[MotorOutputNumber] = Speed;
 			return true;
 		} else {
+			MotorErrorHandler();
 			return false;
 		}
 	}
@@ -53,6 +62,7 @@ bool Motor(uint8_t MotorOutputNumber, uint8_t Direction, uint8_t Speed)
 			MotorDriverActualSpeed[MotorOutputNumber] = Speed;
 			return true;
 		} else {
+			MotorErrorHandler();
 			return false;
 		}
 	}
@@ -63,6 +73,7 @@ bool Motor(uint8_t MotorOutputNumber, uint8_t Direction, uint8_t Speed)
 			MotorDriverActualDirection[MotorOutputNumber] = Direction;
 			return true;
 		} else {
+			MotorErrorHandler();
 			return false;
 		}
 	}
@@ -73,6 +84,7 @@ bool Motor(uint8_t MotorOutputNumber, uint8_t Direction, uint8_t Speed)
 			MotorDriverActualDirection[MotorOutputNumber] = Direction;
 			return true;
 		} else {
+			MotorErrorHandler();
 			return false;
 		}
 	}
@@ -86,18 +98,75 @@ bool Motor(uint8_t MotorOutputNumber, uint8_t Direction, uint8_t Speed)
 
 bool ControlMotor_CW(uint8_t MotorOutputNumber)
 {
+	bool TempStatus1 = false;
+	bool TempStatus2 = false;
+
 	switch(MotorOutputNumber){
 
 	case Motor1:
-		IoWrite(OUT_3, true);				// OUT NEEDS TO BE CHANGED!!!
-		IoWrite(OUT_4, false);				// OUT NEEDS TO BE CHANGED!!!
-		return true;
+		if (MotorOutputStatus[0] != 1){
+			if (IoWrite(OUT_3, true)){				// OUT NEEDS TO BE CHANGED!!!
+				MotorOutputStatus[0] = 1;
+				TempStatus1 = true;
+			}
+			else {
+				return false;
+			}
+		} else if(FirstMotorStart[Motor1]){
+			FirstMotorStart[Motor1] = false;
+			TempStatus1 = true;
+		}
+
+		if (MotorOutputStatus[1] != 0){
+			if (IoWrite(OUT_4, false)){				// OUT NEEDS TO BE CHANGED!!!
+				MotorOutputStatus[1] = 0;
+				TempStatus2 = true;
+
+			} else{
+				return false;
+			}
+		}  else if(FirstMotorStart[Motor1]){
+			FirstMotorStart[Motor1] = false;
+			TempStatus2 = true;
+		}
+
+		if (TempStatus1 && TempStatus2){
+			return true;
+		}
+
 		break;
 
 	case Motor2:
-		IoWrite(OUT_5, true);				// OUT NEEDS TO BE CHANGED!!!
-		IoWrite(OUT_6, false);				// OUT NEEDS TO BE CHANGED!!!
-		return true;
+		if (MotorOutputStatus[2] != 1){
+			if (IoWrite(OUT_5, true)){				// OUT NEEDS TO BE CHANGED!!!
+				MotorOutputStatus[2] = 1;
+				TempStatus1 = true;
+			}
+			else {
+				return false;
+			}
+		} else if(FirstMotorStart[Motor2]){
+			FirstMotorStart[Motor2] = false;
+			TempStatus1 = true;
+		}
+
+		if (MotorOutputStatus[3] != 0){
+			if (IoWrite(OUT_6, false)){				// OUT NEEDS TO BE CHANGED!!!
+				MotorOutputStatus[3] = 0;
+				TempStatus2 = true;
+
+			} else{
+				return false;
+			}
+		}  else if(FirstMotorStart[Motor2]){
+			FirstMotorStart[Motor2] = false;
+			TempStatus2 = true;
+		}
+
+		if (TempStatus1 && TempStatus2){
+			return true;
+		}
+
 		break;
 
 	case Stepper:
@@ -114,19 +183,77 @@ bool ControlMotor_CW(uint8_t MotorOutputNumber)
 
 bool ControlMotor_CCW(uint8_t MotorOutputNumber)
 {
+	bool TempStatus1 = false;
+	bool TempStatus2 = false;
+
 	switch(MotorOutputNumber){
 
 	case Motor1:
-		IoWrite(OUT_3, false);				// OUT NEEDS TO BE CHANGED!!!
-		IoWrite(OUT_4, true);				// OUT NEEDS TO BE CHANGED!!!
-		return true;
+		if (MotorOutputStatus[0] != 0){
+			if (IoWrite(OUT_3, false)){				// OUT NEEDS TO BE CHANGED!!!
+				MotorOutputStatus[0] = 0;
+				TempStatus1 = true;
+			}
+			else {
+				return false;
+			}
+		} else if(FirstMotorStart[Motor1]){
+			FirstMotorStart[Motor1] = false;
+			TempStatus1 = true;
+		}
+
+		if (MotorOutputStatus[1] != 1){
+			if (IoWrite(OUT_4, true)){				// OUT NEEDS TO BE CHANGED!!!
+				MotorOutputStatus[1] = 1;
+				TempStatus2 = true;
+
+			} else{
+				return false;
+			}
+		}  else if(FirstMotorStart[Motor1]){
+			FirstMotorStart[Motor1] = false;
+			TempStatus2 = true;
+		}
+
+		if (TempStatus1 && TempStatus2){
+			return true;
+		}
+
 		break;
 
 	case Motor2:
-		IoWrite(OUT_5, false);				// OUT NEEDS TO BE CHANGED!!!
-		IoWrite(OUT_6, true);				// OUT NEEDS TO BE CHANGED!!!
-		return true;
+		if (MotorOutputStatus[2] != 0){
+			if (IoWrite(OUT_5, false)){				// OUT NEEDS TO BE CHANGED!!!
+				MotorOutputStatus[2] = 0;
+				TempStatus1 = true;
+			}
+			else {
+				return false;
+			}
+		} else if(FirstMotorStart[Motor2]){
+			FirstMotorStart[Motor2] = false;
+			TempStatus1 = true;
+		}
+
+		if (MotorOutputStatus[3] != 1){
+			if (IoWrite(OUT_6, true)){				// OUT NEEDS TO BE CHANGED!!!
+				MotorOutputStatus[3] = 1;
+				TempStatus2 = true;
+
+			} else{
+				return false;
+			}
+		}  else if(FirstMotorStart[Motor2]){
+			FirstMotorStart[Motor2] = false;
+			TempStatus2 = true;
+		}
+
+		if (TempStatus1 && TempStatus2){
+			return true;
+		}
+
 		break;
+
 
 	case Stepper:
 		StepperStateVariable[0] = Stepper_CCW;
@@ -234,5 +361,8 @@ uint8_t StepperState (uint8_t StepperNumber)
 
 // Set all Motor variables back to default.
 void MotorErrorHandler(void){
+	ControlMotor_Stop(Motor1);
+	ControlMotor_Stop(Motor2);
 	StepperStateVariable[0] = Stepper_Idle;
+	MotordriverIni();
 }
